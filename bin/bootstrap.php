df7a358d056e926e05fe8fb6c4197ebdb803f5b8
Ashur Cabrera <hello@cabreramade.co>
2017-10-15 20:22:49 -0700

    🚧 Adding filter and corpora registration middleware

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -75,6 +75,77 @@ $___bootstrap = function( Shell\Application &$app )
 	};
 	$app->pushMiddleware( new Middleware\Middleware( $___init ) );
 
+	/**
+	 * Registers filters and corpora
+	 *
+	 * @param	Cranberry\Shell\Input\InputInterface	$input
+	 *
+	 * @param	Cranberry\Shell\Output\OutputInterface	$output
+	 *
+	 * @param	ZTB\Engine	$engine
+	 *
+	 * @todo	Populate $unwantedWords
+	 *
+	 * @return	Middleware\Middleware::CONTINUE
+	 */
+	$___register = function( Input\InputInterface $input, Output\OutputInterface $output, Engine $engine )
+	{
+		/*
+		 * Filters
+		 */
+		$engine->registerGlobalFilter( [$engine, '___filterHyphens'], [1] );
+		$engine->registerGlobalFilter( [$engine, '___filterSpaces'], [0] );
+
+		// TODO
+		$unwantedWords = [];
+		$engine->registerGlobalFilter( [$engine, '___filterUnwantedWords'], [$unwantedWords] );
+
+		/*
+		 * Corpora
+		 */
+		$dataPathname = $input->getEnv( 'ZTB_DATA' );
+		$dataDirectory = new Filesystem\Directory( $dataPathname );
+
+		$configFile = $dataDirectory->getChild( 'config.json', Filesystem\Node::FILE );
+		if( !$configFile->exists() )
+		{
+			throw new \RuntimeException( sprintf( "Invalid configuration: '%s' not found", $configFile->getPathname() ) );
+		}
+		$config = json_decode( $configFile->getContents(), true );
+		if( json_last_error() != JSON_ERROR_NONE )
+		{
+			throw new \RuntimeException( sprintf( "Invalid configuration: '%s' in %s", json_last_error_msg(), $configFile->getPathname() ) );
+		}
+
+		foreach( ['first_names','last_names'] as $corpusPool )
+		{
+			if( !isset( $config[$corpusPool] ) )
+			{
+				throw new \RuntimeException( sprintf( "Invalid configuration: Missing required key '%s' in %s", $corpusPool, $configFile->getPathname() ) );
+			}
+
+			foreach( $config[$corpusPool] as $corpusInfo )
+			{
+				$corpusDomain = isset( $corpusInfo['domain'] ) ? $corpusInfo['domain'] : null;
+				$corpus = $engine->getCorpus( $corpusInfo['category'], $corpusInfo['corpus'], $corpusDomain );
+
+				switch( $corpusPool )
+				{
+					case 'first_names':
+						$engine->registerFirstNameCorpus( $corpus );
+						break;
+
+					case 'last_names':
+						$engine->registerLastNameCorpus( $corpus );
+						break;
+				}
+			}
+		}
+
+		return Middleware\Middleware::CONTINUE;
+	};
+	$app->pushMiddleware( new Middleware\Middleware( $___register ) );
+
 	/*
 	 * Commands
 	 */
